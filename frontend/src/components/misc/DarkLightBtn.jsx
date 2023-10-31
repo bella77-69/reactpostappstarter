58090a2e6ae780a542261a82f6a6ab0f5476a6b3
chantelle mckenzie <mrspasceri@hotmail.com>
2023-10-30 18:38:52 -0700

    first commit

---
+++ frontend/src/components/misc/DarkLightBtn.jsx
@@ -0,0 +1,39 @@
+import cx from "clsx";
+import {
+  Switch,
+  useMantineColorScheme,
+  useComputedColorScheme,
+  Group,
+} from "@mantine/core";
+import classes from "./DarkLightBtn.module.css";
+
+export function DarkLightBtn() {
+  const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme("light", {
+    getInitialValueInEffect: true,
+  });
+
+  return (
+    <Group justify="center">
+      <Switch
+        onClick={() =>
+          setColorScheme(computedColorScheme === "light" ? "dark" : "light")
+        }
+        variant="default"
+        aria-label="Toggle color scheme"
+        classNames={classes}
+      >
+        {/* <IconSun
+          className={cx(
+            classes.icon,
+            computedColorScheme === "light" ? classes.light : classes.dark
+          )}
+          stroke={1.5}
+        /> */}
+        {/* {computedColorScheme === "dark" && (
+          <IconMoon className={cx(classes.icon, classes.light)} stroke={1.5} />
+        )} */}
+      </Switch>
+    </Group>
+  );
+}
